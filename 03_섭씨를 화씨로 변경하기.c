f05ccb4d43711c2e749d022c1293ffd5b3fea075
BumGyo <89345976+BumGyo@users.noreply.github.com>
2023-09-13 17:16:16 +0900

    Add files via upload

---
+++ 03_섭씨를 화씨로 변경하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	double c, f;
+
+	printf("화씨값을 입력하시오: ");
+	scanf("%lf", &f);
+
+	c = (5.0 / 9.0) * (f - 32.0);
+	printf("섭씨값은 %lf도 입니다.", c);
+
+	return 0;
+}

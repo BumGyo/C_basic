e24c6ac53e378be3b8d62dd44b8b038e079a7c04
BumGyo <89345976+BumGyo@users.noreply.github.com>
2023-09-13 16:54:04 +0900

    Add files via upload

---
+++ 01_사칙연산.c
@@ -0,0 +1,13 @@
+// 두수의 합, 차, 곱 나눗셈의 결과값을 출력하는 프로그램
+
+#include <stdio.h>
+
+int main(void) {
+	int x = 20, y = 10;
+	printf("두 수의 합: %d\n", x + y);
+	printf("두 수의 차: %d\n", x - y);
+	printf("두 수의 곱: %d\n", x * y);
+	printf("두 수의 몫: %d\n", x / y);
+
+	return 0;
+}

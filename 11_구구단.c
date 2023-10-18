c7f4ae30eca14d322f05133869a497d389d53eef
BumGyo <89345976+BumGyo@users.noreply.github.com>
2023-10-18 16:31:13 +0900

    Add files via upload

--- 11_구구단.c
+++ 11_구구단.c
@@ -6,11 +6,13 @@ int main(void) {
 	int dan;
 	int i = 1;
 
-	// 문자 하나 입력 받기
+	// 구구단의 단 입력 받기
 	printf("2~9 사이의 정수를 입력 : ");
 	scanf_s("%d", &dan);
 
+	// 조건문을 활용해 단의 범위 확인해주기
 	if (dan >= 2 && dan <= 9) {
+		// while문을 통해 구구단 생성
 		while (i <= 9) {
 			printf("%d * %d = %d\n", dan, i, dan * i);
 			i++;
